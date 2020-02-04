043ed9af24f1cb96c68439f779f12574e60e2254
GiacomoMeloni <giacomo.meloni.mail@gmail.com>
2020-02-04 16:24:38 +0100

    fade actions

--- src/data/fade/fadeActions.js
+++ src/data/fade/fadeActions.js
@@ -8,6 +8,27 @@ const fadeActions = {
       key: key,
       entry: entry
     })
+  },
+  changeDirection (key, direction) {
+    animationDispatcher.dispatch({
+      type: fadeActionTypes.CHANGE_DIRECTION,
+      key: key,
+      direction: direction
+    })
+  },
+  changeDuration (key, duration) {
+    animationDispatcher.dispatch({
+      type: fadeActionTypes.CHANGE_DURATION,
+      key: key,
+      duration: duration
+    })
+  },
+  changeOpacityLimit (key, opacity) {
+    animationDispatcher.dispatch({
+      type: fadeActionTypes.CHANGE_OPACITY,
+      key: key,
+      opacity: opacity
+    })
   }
 }
 
