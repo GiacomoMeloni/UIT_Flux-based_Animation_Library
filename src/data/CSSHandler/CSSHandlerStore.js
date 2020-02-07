0aa67ae1fe3bee4cc54c91cf11888da24f62b7cd
Giacomo <jackm.medda@gmail.com>
2020-02-07 02:52:28 +0100

    Commented animated.css and fixed adding of css rules

--- src/data/CSSHandler/CSSHandlerStore.js
+++ src/data/CSSHandler/CSSHandlerStore.js
@@ -16,17 +16,12 @@ class CSSHandlerStore extends ReduceStore {
     switch (action.type) {
       case CSSHandlerActionTypes.INSERT_RULE: {
         let index
+
         // Called only if state.sheet is not defined yet
         if ('sheet' in action) {
           index = action.sheet.cssRules.length
           action.sheet.insertRule(action.rule, index)
-          console.log(state.rules.set(
-            action.id,
-            rule({
-              index: index,
-              rule: action.rule
-            })
-          ))
+
           return state.set('sheet', action.sheet).set('rules', state.rules.set(
             action.id,
             rule({
@@ -40,9 +35,7 @@ class CSSHandlerStore extends ReduceStore {
           index = state.rules.get(action.id).index
           state.sheet.insertRule(action.rule, index)
 
-          return state.updateIn(['rules', action.id], value => {
-            value.set('rule', action.rule)
-          })
+          return state.setIn(['rules', action.id, 'rule'], action.rule)
         } else {
           index = state.sheet.cssRules.length
           state.sheet.insertRule(action.rule, index)
